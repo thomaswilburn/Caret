27bd4847a4f57e0c36d602a5aae2063a72092b4d
Thomas Wilburn <news@thomaswilburn.net>
2013-10-07 18:09:35 -0700

    Added selections for word, paragraph, and matching brackets. See issue #48

--- js/aceBindings.js
+++ js/aceBindings.js
@@ -2,13 +2,50 @@ define(["command", "editor", "statusbar"], function(command, editor, status) {
 
     //this is a place to put bindings that don't have direct equivalents in Ace, but are required for Sublime compatibility
 
+    command.on("sublime:expand-to-word", function() {
+      editor.execCommand("gotowordleft");
+      editor.execCommand("selectwordright");
+    });
+
     command.on("sublime:expand-to-line", function() {
       editor.execCommand("gotolinestart");
       editor.execCommand("selecttolineend");
     });
+    
+    command.on("sublime:expand-to-paragraph", function() {
+      var session = editor.getSession();
+      var selection = editor.getSelection();
+      var currentLine = editor.getCursorPosition().row;
+      var startLine = currentLine;
+      var endLine = currentLine;
+      while (startLine > 0) {
+        startLine--;
+        var line = session.getLine(startLine);
+        if (line == "") {
+          //we'll skip the preceding space
+          startLine += 1;
+          break;
+        }
+      }
+      var length = session.getLength();
+      while (endLine < length) {
+        endLine++;
+        var line = session.getLine(endLine);
+        if (line == "") {
+          break;
+        }
+      }
+      editor.clearSelection();
+      editor.moveCursorTo(startLine);
+      selection.selectTo(endLine);
+    });
+    
+    command.on("sublime:expand-to-matching", function() {
+      editor.execCommand("jumptomatching");
+      editor.execCommand("selecttomatching");
+    })
 
     //we also add a command redirect for firing Ace commands via regular command attributes
-
     command.on("ace:command", function(cmd) {
       editor.execCommand(cmd);
     });
