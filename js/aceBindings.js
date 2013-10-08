03524a9ef09f9d4bbed06e5aa6a8585ae9bdc5d2
Thomas Wilburn <news@thomaswilburn.net>
2013-10-08 09:37:19 -0700

    Ace has a command we can use directly instead of shimming Sublime's "expand to word"

--- js/aceBindings.js
+++ js/aceBindings.js
@@ -2,11 +2,6 @@ define(["command", "editor", "statusbar"], function(command, editor, status) {
 
     //this is a place to put bindings that don't have direct equivalents in Ace, but are required for Sublime compatibility
 
-    command.on("sublime:expand-to-word", function() {
-      editor.execCommand("gotowordleft");
-      editor.execCommand("selectwordright");
-    });
-
     command.on("sublime:expand-to-line", function() {
       editor.execCommand("gotolinestart");
       editor.execCommand("selecttolineend");
