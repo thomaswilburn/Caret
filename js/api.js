d31049046d6a2b81f039a83a285042d1a9179d7c
Thomas Wilburn <news@thomaswilburn.net>
2013-12-02 15:38:12 -0800

    Provide sample API message, add a menu item for api.json

--- js/api.js
+++ js/api.js
@@ -8,9 +8,12 @@ define(["command", "settings!api"], function(command, Settings) {
   command.on("api:execute", function(id) {
     if (!id in targets) return;
     var config = targets[id];
-    chrome.runtime.sendMessage(config.id, config.message);
+    chrome.runtime.sendMessage(config.id, config.message, null, function() {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+      }
+    });
   });
-  
   
   //External apps can send messages by matching Caret's command/argument config objects
   chrome.runtime.onMessageExternal.addListener(function(message, sender, c) {
