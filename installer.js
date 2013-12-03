714d7de3e9b8801188aa0aa88c8a5374f90c0257
Thomas Wilburn <news@thomaswilburn.net>
2013-12-02 16:35:50 -0800

    Added notes to installer.js, since we'll need it soon.
    Related to #79.

--- installer.js
+++ installer.js
@@ -7,5 +7,14 @@ chrome.runtime.onInstalled.addListener(function(e) {
   var minor = semver[1];
   var build = semver[2];
   
+  /*
+  
+  As with Android database upgrades, we'll perform these as a series of if statements, ordered by increasing
+  version number. We should also provide a notification that the system is upgrading, and prevent opening new
+  windows until the process finishes. In theory, this script shares a document with background.js, so they can
+  just use a common flag to halt the openWindow process during upgrades.
+  
+  */
+  
   
 });
