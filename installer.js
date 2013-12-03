94fe3acbb7b3b4909b24451c2147e99afcd9144b
Thomas Wilburn <news@thomaswilburn.net>
2013-12-03 08:28:34 -0800

    Start backing up settings files to syncFileSystem in installer.js

--- installer.js
+++ installer.js
@@ -1,4 +1,4 @@
-chrome.runtime.onInstalled.addListener(function(e) {
+//chrome.runtime.onInstalled.addListener(function(e) {
   //this is where we'll track upgrades
   if (!e.previousVersion) return;
   
@@ -15,6 +15,56 @@ chrome.runtime.onInstalled.addListener(function(e) {
   just use a common flag to halt the openWindow process during upgrades.
   
   */
+
+  var process = {
+    count: 0,
+    start: function() {
+      this.count++;
+      //show notification while upgrading
+    },
+    finish: function() {
+      this.count--;
+      if (this.count == 0) {
+        console.log("All upgrades complete");
+        //hide notification when all processes finish.
+        //if a window open request was filed, call openWindow and clear the flag
+        //can use timeout as the flag, if we want, but should probably rename as pending
+      }
+    }
+  };
+
+  //start backing up settings to syncFileSystem
+  if (true) {
+    process.start();
+    chrome.storage.sync.get(function(sync) {
+      var saved = {};
+      var check = function() {
+        for (var name in sync) {
+          console.log(name);
+          if (!(name in saved)) {
+            return;
+          }
+        }
+        process.finish();
+      }
+      chrome.syncFileSystem.requestFileSystem(function(fs) {
+        window.fs = fs;
+        var root = fs.root;
+        for (var name in sync) {
+          root.getFile(name, {create: true}, function(f) {
+            f.createWriter(function(writer) {
+              writer.onwriteend = function() {
+                writer.onwriteend = check;
+                writer.write(new Blob([sync[name]]));
+              };
+              writer.truncate(0);
+            });
+          }, function() {
+            process.fail("http://example.com");
+          };
+        }
+      });
+    });
+  }
   
-  
-});
+//});
