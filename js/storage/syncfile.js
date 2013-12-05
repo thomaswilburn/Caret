5b4a3e22f7f769d8c869e315c3b3a41e0582d951
Thomas Wilburn <news@thomaswilburn.net>
2013-12-04 23:34:49 -0800

    Removed window.sync, added some comments.

--- js/storage/syncfile.js
+++ js/storage/syncfile.js
@@ -2,8 +2,23 @@ define([
     "storage/syncFS",
     "command"
   ], function(sync, command) {
-    
-  window.sync = sync;
+
+  /*
+
+  SyncFile is a wrapper around Chrome's synchronized storage use the same
+  interface as the File module. This means you can assign a SyncFile
+  containing Caret's settings JSON to a tab just as you would a regular file,
+  and the tab can read/write from it without any customization. The one
+  difference is the virtual flag, which is used to prevent some file-only
+  behaviors (such as retainEntry calls).
+
+  As soon as chrome.syncFileSystem allows us to create a filesystem even if
+  the device is offline for the first request, we'll swap these over to a
+  wrapper around file entries from that storage. The change should remain
+  entirely transparent as far as other modules are concerned, although there
+  are some abstraction leaks in settings.js that we'll need to handle.
+  
+  */
     
   var SyncFile = function(name, c) {
     this.entry = {};
