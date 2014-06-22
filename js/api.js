a584b37d5b4fd002ef747cc803e114d16f3672c9
Thomas Wilburn <news@thomaswilburn.net>
2014-06-22 00:13:11 -0700

    Added editor context to API execution. Fixes #220

--- js/api.js
+++ js/api.js
@@ -12,12 +12,28 @@ define([
   command.on("api:execute", function(id, c) {
     if (!id in targets) return c();
     var config = targets[id];
-    chrome.runtime.sendMessage(config.id, config.message, null, function() {
-      if (chrome.runtime.lastError) {
-        console.error(chrome.runtime.lastError);
-      }
-      if (c) c();
-    });
+    var message = Object.create(config.message);
+    var send = function() {
+      chrome.runtime.sendMessage(config.id, message, null, function() {
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError);
+        }
+        if (c) c(chrome.runtime.lastError);
+      });
+    };
+    if (config.sendEditorContext) {
+      //add context information to the message
+      editor.session.file.getPath(function(err, path) {
+        message.context = {
+          path: path,
+          selection: editor.session.getTextRange()
+        };
+        send();
+      });
+    } else {
+      //send message as-is
+      send();
+    }
   });
   
   //External apps can send messages by matching Caret's command/argument config objects
