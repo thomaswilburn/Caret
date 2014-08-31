1597f6bd92ee29a18d9ad704d705c237db04a54f
Thomas Wilburn <news@thomaswilburn.net>
2014-08-30 23:01:02 -0700

    Fixes #279

--- js/aceBindings.js
+++ js/aceBindings.js
@@ -31,9 +31,10 @@ define([
     }
 
     //this is a place to put bindings that don't have direct equivalents in Ace, but are required for Sublime compatibility
+    
+    //this is now in Ace, but aliased for back-compat
     command.on("sublime:expand-to-line", function(c) {
-      editor.execCommand("gotolinestart");
-      editor.execCommand("selecttolineend");
+      editor.execCommand("expandtoline");
       if (c) c();
     });
 
