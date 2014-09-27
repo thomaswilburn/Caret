d41137d85fa3b3399adb51a0de19ba161c3813fe
xran-deex <randy.valis@gmail.com>
2014-09-26 21:47:27 -0400

    Added draggable divider

---
+++ js/ui/divider.js
@@ -0,0 +1,55 @@
+require([
+  'command',
+  'util/dom2'
+  ], function(command){
+  
+  // Keep track of the dimensions of the project sidebar
+
+  var xCord, projectWidth;
+  
+  var project = document.find('.project');
+  var divider = document.find('.divider');
+  var body = document.find('body');
+  
+  /*
+   * \fn eventListener
+   *
+   *  Initiate dragging on mouse down.
+   */
+  divider.addEventListener('mousedown', function(e){
+    // Don't listen on center button clicks
+    if(e.button === 2){
+      return;
+    }
+    // get the initial x coordinates
+    xCord = e.screenX;
+    projectWidth = project.offsetWidth;
+    
+    // add the mousemove and mouseup listeners
+    body.addEventListener('mousemove', moveListener);
+    body.addEventListener('mouseup', upListener);
+    
+  });
+  
+  /*
+   * \fn moveListener
+   *
+   *  Change the project bar width on drag.
+   */
+  function moveListener(e){
+      var diff = e.screenX - xCord;
+      projectWidth += diff;
+      project.css('width', projectWidth + 'px');
+      xCord = e.screenX;
+  }
+
+  /*
+   * \fn upListener
+   *
+   *  Remove the mousemove and mouseup listeners on mouse up.
+   */
+  function upListener(e){
+      body.removeEventListener('mousemove', moveListener, false);
+      body.removeEventListener('mouseup', this, false);
+  }
+});
