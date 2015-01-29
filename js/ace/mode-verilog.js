713a786febe8619bf1b17ecd3ca5fdff7f322e44
fordred <fordred@users.noreply.github.com>
2015-01-29 16:27:09 +0000

    Add new SV keywords.

--- js/ace/mode-verilog.js
+++ js/ace/mode-verilog.js
@@ -5,7 +5,7 @@ var oop = require("../lib/oop");
 var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
 
 var VerilogHighlightRules = function() {
-var keywords = "always|and|assign|automatic|begin|buf|bufif0|bufif1|case|casex|casez|cell|cmos|config|" +
+var keywords = "always|always_comb|always_ff|and|assign|automatic|begin|buf|bufif0|bufif1|case|casex|casez|cell|cmos|config|" +
     "deassign|default|defparam|design|disable|edge|else|end|endcase|endconfig|endfunction|endgenerate|endmodule|" +
     "endprimitive|endspecify|endtable|endtask|event|for|force|forever|fork|function|generate|genvar|highz0|" +
     "highz1|if|ifnone|incdir|include|initial|inout|input|instance|integer|join|large|liblist|library|localparam|" +
