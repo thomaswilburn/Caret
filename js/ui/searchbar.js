42e27effd55ab4681efdf9082e3dc1cdaaf43da3
Brian Smith <brismuth@gmail.com>
2015-10-26 00:51:46 -0600

    Making search results clickable Adding isReadOnly to tabs Adding optional callback to openFile

--- js/ui/searchbar.js
+++ js/ui/searchbar.js
@@ -27,6 +27,7 @@ define([
 
     this.bindInput();
     this.bindButtons();
+    this.bindSearchResultClicks();
   };
 
   Searchbar.prototype = {
@@ -63,6 +64,26 @@ define([
       });
     },
 
+    bindSearchResultClicks: function() {
+      // catch the click events on search results
+      editor.on("click", function(e) {
+        var currentTab = sessions.getCurrent();
+        if (!currentTab.lineLinks) { // this must not be a search result tab
+          return;
+        }
+
+        var editor = e.editor;
+        var line = editor.getCursorPosition().row;
+        var lineLink = currentTab.lineLinks[line];
+        if (lineLink) {
+          var file = project.openFile(lineLink.nodeEntry.fullPath, function() {
+            // editor is now the newly opened file
+            editor.gotoLine(lineLink.row);
+          });
+        }
+      });
+    },
+
     // todo add regex support
     // todo add search history
     // we don't have to worry about the files blacklist because they are already removed from the project structure
@@ -75,8 +96,7 @@ define([
       var isCaseSensitive = this.element.find("#search-case-check").checked;
       var displayQuery = this.input.value;
 
-      var resultsTab = sessions.addFile(i18n.get("searchDisplayQuery", displayQuery));
-      resultsTab.file = new NullFile();
+      var resultsTab = this.createResultsTab(displayQuery);
 
       this.currentSearch = {
         matches: 0,
@@ -84,11 +104,6 @@ define([
         searchQuery: new RegExp(displayQuery, isCaseSensitive ? "g" : "ig"),
         resultsTab: resultsTab
       };
-
-      resultsTab.fileName = i18n.get("searchTabName", displayQuery);
-      resultsTab.addEventListener("close", function() {
-        self.currentSearch.running = false;
-      });
 
       var fileEntryList = this.getFlatFileEntryList();
       if (fileEntryList.length < 1) { // exit early if there are no directories added
@@ -145,19 +160,25 @@ define([
       var options = this.currentSearch;
 
       chrome.fileSystem.getDisplayPath(nodeEntry, function(path) {
-        var file = new File(nodeEntry);
         if (!options.running) {
           return c();
         }
 
-        var printResult = function(index, str) {
-          self.appendToResults(self.formatResultCode(index, str));
-        };
+        var file = new File(nodeEntry);
 
         file.read(function(err, data) {
+          if (!options.running) {
+            return c();
+          }
+
           var lines = data.split("\n");
           var firstFindInFile = true;
           var printedLines = {}; // only print each line once per file per search
+          var printResult = function(index, str) {
+            printedLines[index] = true;
+            var result = "  " + (index + 1) + ": " + str;
+            self.appendToResults(result, nodeEntry, index+1);
+          };
 
           for (var i = 0; i < lines.length; i++) {
             line = lines[i];
@@ -170,25 +191,23 @@ define([
               }
 
               if (firstFindInFile) { // only add a filename if it is the first result for the file
-                self.appendToResults("\n" + nodeEntry.fullPath + "\n");
+                self.appendToResults('\n');
+                self.appendToResults(nodeEntry.fullPath, nodeEntry);
                 firstFindInFile = false;
               } else if (!printedLines[i] && !printedLines[i-1] && !printedLines[i-2]) { // add break if immediately previous lines not included
-                self.appendToResults("...\n");
+                self.appendToResults("...");
               }
 
               if (!printedLines[i-1] && i > 1) { // don't print line number 0
                 printResult(i-1, lines[i-1]);
-                printedLines[i-1] = true;
               }
 
               if (!printedLines[i]) {
                 printResult(i, lines[i]);
-                printedLines[i] = true;
               }
 
               if (i < lines.length - 1) { // always print the line following the search result, if it exists
                 printResult(i+1, lines[i+1]);
-                printedLines[i+1] = true;
               }
             }
           }
@@ -196,6 +215,22 @@ define([
           c();
         });
       });
+    },
+
+    createResultsTab: function(displayQuery) {
+      var self = this;
+
+      var resultsTab = sessions.addFile(i18n.get("searchDisplayQuery", displayQuery));
+      resultsTab.file = new NullFile();
+      resultsTab.lineLinks = {};
+      resultsTab.fileName = i18n.get("searchTabName", displayQuery);
+      resultsTab.addEventListener("close", function() {
+        self.currentSearch.running = false;
+      });
+      resultsTab.isReadOnly = true;
+      editor.setReadOnly(true);
+
+      return resultsTab;
     },
 
     printSearchSummary: function(searchedEverything, filesScanned) {
@@ -206,19 +241,22 @@ define([
       this.currentSearch.running = false;
     },
 
-    formatResultCode: function(lineNumber, code) {
-      return "  " + (lineNumber + 1) + ": " + code + "\n";
-    },
-
-    appendToResults: function(text) {
+    appendToResults: function(text, nodeEntry, row) {
       if (text === "") {
         return;
       }
 
       var resultsTab = this.currentSearch.resultsTab;
       var insertRow = resultsTab.doc.getLength();
-      resultsTab.doc.insert({row: insertRow, column: 0}, text);
+      resultsTab.doc.insert({row: insertRow, column: 0}, text + "\n");
       resultsTab.modified = false;
+
+      if (nodeEntry) {
+        resultsTab.lineLinks[insertRow-1] = {
+          nodeEntry: nodeEntry,
+          row: row || 1
+        };
+      }
     },
 
     activate: function(mode) {
